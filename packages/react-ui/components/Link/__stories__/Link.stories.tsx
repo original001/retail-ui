b7a01222c5c344e3e338e5303caf06cdb34e687e
Mikhail <dasdgool@gmail.com>
2020-04-03 13:14:23 +0500

    chore(link): add new tests, approve new screenshots

--- packages/react-ui/components/Link/__stories__/Link.stories.tsx
+++ packages/react-ui/components/Link/__stories__/Link.stories.tsx
@@ -20,6 +20,35 @@ const linkTests: CreeveyStoryParams['tests'] = {
       .perform();
     await this.expect(await this.takeScreenshot()).to.matchImage('hover');
   },
+  async pressedThenReleased() {
+    const element = await this.browser.findElement({ css: '#test-element' });
+    const link = await this.browser.findElement({ css: '[data-comp-name~=Link]' });
+
+    const idle = await element.takeScreenshot();
+
+    await this.browser
+      .actions({ bridge: true })
+      .move({ origin: link })
+      .perform();
+
+    const hover = await element.takeScreenshot();
+
+    await this.browser
+      .actions({ bridge: true })
+      .press()
+      .perform();
+
+    const pressed = await element.takeScreenshot();
+
+    await this.browser
+      .actions({ bridge: true })
+      .release()
+      .perform();
+
+    const released = await element.takeScreenshot();
+
+    await this.expect({ idle, hover, pressed, released }).to.matchImages();
+  },
 };
 
 export default { title: 'Link', parameters: { creevey: { skip: [{ in: ['ie11', 'ie11Flat'], tests: 'hover' }] } } };
@@ -43,5 +72,56 @@ Grayed.story = { parameters: { creevey: { tests: linkTests } } };
 export const Disabled: CSFStory<JSX.Element> = () => <Link disabled>Simple link</Link>;
 Disabled.story = { parameters: { creevey: { tests: linkTests } } };
 
-export const WithOnClick = () => <Link onClick={() => Toast.push('Clicked!')}>Simple Link</Link>;
-WithOnClick.story = { name: 'With onClick', parameters: { creevey: { skip: [true] } } };
+export const WithSpaces: CSFStory<JSX.Element> = () => {
+  return <Link>Link with spaces</Link>;
+};
+WithSpaces.story = { parameters: { creevey: { tests: linkTests } } };
+
+export const Success: CSFStory<JSX.Element> = () => {
+  return <Link use="success">Link with spaces</Link>;
+};
+Success.story = { parameters: { creevey: { tests: linkTests } } };
+
+export const WithOnClick: CSFStory<JSX.Element> = () => <Link onClick={() => Toast.push('Clicked!')}>Simple Link</Link>;
+WithOnClick.story = {
+  name: 'With onClick',
+  parameters: {
+    creevey: {
+      tests: {
+        async hover() {
+          // 1. находим элемент для скриншота
+          const element = await this.browser.findElement({ css: '#test-element' });
+
+          // находим link
+          const link = await this.browser.findElement({ css: '[data-comp-name*=Link]' });
+
+          // 2. делаем скриншот "по умолчанию"
+          const idle = await element.takeScreenshot();
+
+          // 3. наводим указатель мыши
+          await this.browser
+            .actions({ bridge: true })
+            .move({ origin: link })
+            .perform();
+
+          // 4. делаем скриншот "при наведении"
+          const hover = await element.takeScreenshot();
+
+          await this.browser
+            .actions({ bridge: true })
+            .click(link)
+            .perform();
+
+          // Находим тост
+          const toast_element = await this.browser.findElement({ css: '[data-tid*=ToastView__root]' });
+
+          // 5. делаем скриншот "при клике"
+          const toast = await toast_element.takeScreenshot();
+
+          // 6. сравниваем результаты
+          await this.expect({ idle, hover, toast }).to.matchImages();
+        },
+      },
+    },
+  },
+};
